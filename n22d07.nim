02941b6c6528cb74f8a8cf843dd617a9240e8c15
genius487 <94225905+genius487@users.noreply.github.com>
2022-12-07 19:07:06 +0100

    Add files via upload

---
+++ n22d07.nim
@@ -0,0 +1,51 @@
+import strutils, tables, strscans, sequtils, algorithm
+
+var
+  terminal = readFile("22day07.txt").strip.splitLines
+  parts = [0,0]
+  directorySizes = initTable[string, int]()
+  usedSpace, spaceToFreeUp: int
+
+proc dirSizeCalc(crsr: int = 0): (int, int) =
+  var
+    cursor = crsr
+    files = initTable[string, int]()
+    fileSize, size, place: int
+    fileName: string
+
+  while cursor <= terminal.high:
+    #echo cursor
+    if "$ cd" in terminal[cursor]:
+      if ".." in terminal[cursor]:
+        fileSize = foldl(files.values.toSeq, a + b)
+        if fileSize <= 100000:
+          parts[0].inc fileSize
+        
+        return (fileSize, cursor)
+      else: 
+        (size, place) = dirSizeCalc(cursor+2)
+        files[terminal[cursor][5..^1]] = size
+        directorySizes[terminal[cursor][5..^1]] = size
+
+        cursor = place
+        #echo cursor
+
+    elif scanf(terminal[cursor], "$i $+", fileSize, fileName):
+      #echo terminal[cursor]
+      files[fileName] = fileSize
+
+    #echo files
+
+    inc cursor
+
+  return (foldl(files.values.toSeq, a + b), cursor)
+
+usedSpace = dirSizeCalc()[0]
+spaceToFreeUp = 30000000 - (70000000 - usedSpace)
+
+for val in directorySizes.values.toSeq.sorted:
+  if val >= spaceToFreeUp:
+    parts[1] = val
+    break
+
+echo parts
