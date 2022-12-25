3fc4945a155581e41badbcaec9ff3531e4a4c2a3
genius487 <94225905+genius487@users.noreply.github.com>
2022-12-25 05:56:38 -0500

    Update n22d04.nim

--- n22d04.nim
+++ n22d04.nim
@@ -10,7 +10,7 @@ for line in ids:
     if (i1 >= i3 and i2 <= i4) or (i3 >= i1 and i4 <= i2):
       parts[0].inc
 
-    if i1 in toSeq(i3..i4) or i2 in toSeq(i3..i4) or i3 in toSeq(i1..i2) or i4 in toSeq(i1..i2):
+    if i1 in i3..i4 or i2 in i3..i4 or i3 in i1..i2 or i4 in i1..i2:
       parts[1].inc
 
 
